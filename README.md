985bade9e1d494a6a4fe2b1ddefaa2adcfce59d3
Rafal Mierzwiak <rm@rm.pl>
2017-08-14 14:51:16 +0100

    serve_and_track, initial version

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 # serve-and-track
 
 A tracking web server.
+
+Main function is to serve an image and log requests in apache log format. Served are also service status (health established based on presence of a state file), and service metrics (via use of Prometheus client library).
+
+* server
+
+```
+$ go run serve_and_track.go
+2017/08/14 14:47:50 INFO http: Server started :8080
+::1 - - [14/Aug/2017:14:47:57 +0100] "GET /state HTTP/1.1" 200 2 "" "curl/7.54.0"
+::1 - - [14/Aug/2017:14:48:06 +0100] "GET /state HTTP/1.1" 503 33 "" "curl/7.54.0"
+::1 - - [14/Aug/2017:14:48:14 +0100] "GET /track HTTP/1.1" 200 42 "" "curl/7.54.0"
+::1 - - [14/Aug/2017:14:48:25 +0100] "GET /metrics HTTP/1.1" 200 5150 "" "curl/7.54.0"
+::1 - - [14/Aug/2017:14:48:36 +0100] "GET /track HTTP/1.1" 200 42 "" "curl/7.54.0"
+::1 - - [14/Aug/2017:14:48:41 +0100] "GET /metrics HTTP/1.1" 200 5150 "" "curl/7.54.0"
+2017/08/14 14:49:11 INFO http: Server stopping
+2017/08/14 14:49:11 INFO http: Server stopped gracefully
+```
+
+* client
+
+```
+$ touch ./state && curl -sS --write-out "http_code: %{http_code}, size_download: %{size_download}" --output /dev/null http://localhost:8080/state
+http_code: 200, size_download: 2
+
+$ rm -f ./state && curl -sS --write-out "http_code: %{http_code}, size_download: %{size_download}" --output /dev/null http://localhost:8080/state
+http_code: 503, size_download: 33
+
+$ curl -sS --write-out "http_code: %{http_code}, size_download: %{size_download}" --output /dev/null http://localhost:8080/track
+http_code: 200, size_download: 42
+
+$ curl -sS http://localhost:8080/metrics | grep "^tracking_"
+tracking_request_duration{quantile="0.5"} 1.9946e-05
+tracking_request_duration{quantile="0.9"} 1.9946e-05
+tracking_request_duration{quantile="0.99"} 1.9946e-05
+tracking_request_duration_sum 1.9946e-05
+tracking_request_duration_count 1
+tracking_requests_count_total{status="success"} 1
+tracking_requests_size_total 42
+
+$ curl -sS http://localhost:8080/track | file -b --mime-type -
+image/gif
+
+$ curl -sS http://localhost:8080/metrics | grep "^tracking_"
+tracking_request_duration{quantile="0.5"} 9.125e-06
+tracking_request_duration{quantile="0.9"} 1.9946e-05
+tracking_request_duration{quantile="0.99"} 1.9946e-05
+tracking_request_duration_sum 2.9071e-05
+tracking_request_duration_count 2
+tracking_requests_count_total{status="success"} 2
+tracking_requests_size_total 84
+```
---
+++ serve_and_track.go
@@ -0,0 +1,186 @@
+/*
+
+A tracking web server.
+
+Main function is to serve an image and log requests in apache log format. Served
+are also service status (health established based on presence of a state file),
+and service metrics (via use of Prometheus client library).
+
+*/
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+// Command line configuration options
+var (
+	listenAddress = kingpin.Flag("listen-address", "Address on which to expose metrics and web interface.").Default(":8080").String()
+
+	trackingURLPath = kingpin.Flag("tracking-url-path", "Path under which to expose tracking image.").Default("/track").String()
+	metricsURLPath  = kingpin.Flag("metrics-url-path", "Path under which to expose service metrics.").Default("/metrics").String()
+	stateURLPath    = kingpin.Flag("state-url-path", "Path under which to expose service state.").Default("/state").String()
+
+	stateFilePath = kingpin.Flag("state-file-path", "File path which indicates service state.").Default("./state").String()
+)
+
+// GIF transparent image to serve as a tracking image
+var GIF = []byte{
+	71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 128, 0, 0, 0, 0, 0,
+	255, 255, 255, 33, 249, 4, 1, 0, 0, 0, 0, 44, 0, 0, 0, 0,
+	1, 0, 1, 0, 0, 2, 1, 68, 0, 59,
+}
+
+// Monitoring metrics
+var (
+	serveImageRequestDuration = prometheus.NewSummary(prometheus.SummaryOpts{
+		Name:       "tracking_request_duration",
+		Help:       "Duration of requests.",
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+	})
+
+	serveImageRequestsSize = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "tracking_requests_size_total",
+		Help: "Size of requests, total.",
+	})
+
+	serveImageRequestsCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "tracking_requests_count_total",
+			Help: "Number of requests served partitioned by status (failure or success).",
+		},
+		[]string{"status"},
+	)
+)
+
+// Initializes service metrics.
+func initMetrics() {
+	prometheus.MustRegister(serveImageRequestDuration)
+	prometheus.MustRegister(serveImageRequestsCount)
+	prometheus.MustRegister(serveImageRequestsSize)
+}
+
+// Initializes the http server.
+func initServer() *http.Server {
+	r := mux.NewRouter()
+
+	r.HandleFunc(*trackingURLPath, serveImage)
+	r.HandleFunc(*stateURLPath, serveState)
+	r.Handle(*metricsURLPath, promhttp.Handler())
+
+	return &http.Server{
+		Addr:    *listenAddress,
+		Handler: handlers.CombinedLoggingHandler(os.Stdout, r)}
+}
+
+// Starts the http server.
+func startServer(srv *http.Server) {
+	log.Println("INFO http: Server started", *listenAddress)
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal("ERROR ", err)
+	}
+}
+
+// Stops the http server.
+func stopServer(srv *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 8*time.Second)
+	defer cancel()
+
+	log.Println("INFO http: Server stopping")
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("INFO http: Server stopped forcefully")
+	} else {
+		log.Println("INFO http: Server stopped gracefully")
+	}
+}
+
+// Measures function execution time.
+func trackServeImageDuration(start time.Time, id string) {
+	elapsed := time.Since(start)
+	serveImageRequestDuration.Observe(float64(elapsed.Seconds()))
+}
+
+// Serves tracking image.
+func serveImage(w http.ResponseWriter, r *http.Request) {
+	defer trackServeImageDuration(time.Now(), "serveImage")
+
+	if r.Method != "GET" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Content-Type", "image/gif")
+
+	if _, err := w.Write(GIF); err != nil {
+		serveImageRequestsCount.WithLabelValues("failure").Inc()
+		return
+	}
+
+	serveImageRequestsCount.WithLabelValues("success").Inc()
+	serveImageRequestsSize.Add(float64(len(GIF)))
+}
+
+// Checks service state: true if service is healthy, false otherwise. Service is
+// considered healthy when stateFilePath is present.
+func serviceHealthy() bool {
+	_, err := os.Stat(*stateFilePath)
+	return err == nil
+}
+
+// Serves service state: http 200 when healthy, http 503 otherwise.
+func serveState(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Content-Type", "text/html")
+
+	if !serviceHealthy() {
+		w.WriteHeader(503)
+		if _, err := w.Write([]byte("Error 503 (Service not available)")); err != nil {
+			log.Println("WARNING", err)
+		}
+		return
+	}
+
+	if _, err := w.Write([]byte("OK")); err != nil {
+		log.Println("WARNING", err)
+	}
+}
+
+func main() {
+	kingpin.Parse()
+
+	terminateServer := make(chan os.Signal)
+	signal.Notify(terminateServer, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	initMetrics()
+
+	srv := initServer()
+
+	go func() {
+		startServer(srv)
+	}()
+
+	<-terminateServer
+
+	stopServer(srv)
+}
